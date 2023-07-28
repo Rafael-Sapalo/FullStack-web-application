a5341ca9f09dc83ca0a9781c055ac576bda881e2
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2023-07-28 09:15:12 +0200

    [ADD] : admin middleware check

--- src/server/api/middleware/auth-middleware.go
+++ src/server/api/middleware/auth-middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
@@ -9,13 +10,26 @@ import (
 
 func Authenticate() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var session = sessions.Default(ctx);
-		var userID = session.Get("user_id");
+		var session = sessions.Default(ctx)
+		var userID = session.Get("user_id")
 		if userID == nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unhautorized"})
-			ctx.Abort();
-			return;
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			ctx.Abort()
+			return
 		}
-		ctx.Next();
+		ctx.Next()
 	}
+}
+
+func IsAdmin(ctx *gin.Context) {
+	var session = sessions.Default(ctx)
+	var id = session.Get("user_id")
+	fmt.Println(id)
+	if id == nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		ctx.Abort()
+		return
+	}
+
+	ctx.Next()
 }
--- try.py
+++ try.py
@@ -2,7 +2,7 @@ import random
 import string
 
 length = 35
-characters = string.ascii_lowercase + string.digits
+characters = string.ascii_lowercase
 
 random_string = ''.join(random.choices(characters, k=length))
 
