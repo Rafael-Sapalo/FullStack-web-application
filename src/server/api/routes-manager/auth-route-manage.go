3a88c438eec2efba8559b704e8837315d9a344cd
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2023-08-04 01:39:54 +0200

    [FIX: route-manager] : fix auth routes manager

--- src/server/api/routes-manager/auth-route-manage.go
+++ src/server/api/routes-manager/auth-route-manage.go
@@ -4,12 +4,16 @@ import (
 	"github.com/Rafael-Sapalo/FullStack-web-application/server/api/middleware"
 	"github.com/Rafael-Sapalo/FullStack-web-application/server/api/routes"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func SetAuthRoutes(router *gin.Engine) {
 	var authRoutes = router.Group("/auth")
 	authRoutes.Use(middleware.RateLimit())
 	{
+		authRoutes.GET("/try", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"msg": "I'm just here to try some features"})
+		})
 		authRoutes.POST("/register", middleware.RegisterMiddleware(), routes.RegisterRoute)
 		authRoutes.POST("/login", routes.LoginRoute)
 		authRoutes.GET("/logout", routes.LogoutRoute)
